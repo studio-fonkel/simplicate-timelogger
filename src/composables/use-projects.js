51f0455076422499621a500b398b82f9ab6aa9c2
Anoesj <anoesjsadraee@gmail.com>
2021-12-08 10:53:47 +0100

    Set loading projects to false by default

--- src/composables/use-projects.js
+++ src/composables/use-projects.js
@@ -2,7 +2,7 @@ import { ref, shallowRef } from 'vue';
 import { axios } from './use-axios.js';
 
 export const availableProjects = ref([]);
-export const loadingAvailableProjects = shallowRef(true);
+export const loadingAvailableProjects = shallowRef(false);
 export const currentProject = shallowRef(null);
 
 function clearProjects () {
@@ -14,6 +14,7 @@ function addProjects (projects) {
 }
 
 export async function fetchAllProjects () {
+  loadingAvailableProjects.value = true;
   const { data: projects } = await axios.get('projects/project');
   clearProjects();
   addProjects(projects.data);
