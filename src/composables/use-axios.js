695e0462bc24a7396e2fd1bfb96a6ed4d32f0b1e
Anoesj <anoesjsadraee@gmail.com>
2021-12-08 10:52:21 +0100

    Set base url in .env

--- src/composables/use-axios.js
+++ src/composables/use-axios.js
@@ -1,10 +1,20 @@
 import axiosPlugin from 'axios';
 
+let tenantURL = import.meta.env.VITE_SIMPLICATE_TENANT_BASE_URL;
+if (!tenantURL) {
+  throw new Error('VITE_SIMPLICATE_TENANT_BASE_URL is not defined');
+}
+if (!tenantURL.endsWith('/')) {
+  tenantURL += '/';
+}
+
+const baseURL = `${tenantURL}/api/v2/`;
+
 export const axios = axiosPlugin.create({
-  baseURL: 'https://demofonkel.simplicate.nl/api/v2/',
+  baseURL,
   timeout: 3000,
   headers: {
-    'Authentication-Key': import.meta.env.VITE_API_KEY,
-    'Authentication-Secret': import.meta.env.VITE_API_SECRET,
-  }
+    'Authentication-Key': import.meta.env.VITE_SIMPLICATE_API_KEY,
+    'Authentication-Secret': import.meta.env.VITE_SIMPLICATE_API_SECRET,
+  },
 });
