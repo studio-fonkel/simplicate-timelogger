0c72b2f221ae999af42eeb7cce0d86897731ba5a
Anoesj <anoesjsadraee@gmail.com>
2021-12-08 10:53:35 +0100

    Check presence of env vars

--- src/composables/use-axios.js
+++ src/composables/use-axios.js
@@ -2,13 +2,17 @@ import axiosPlugin from 'axios';
 
 let tenantURL = import.meta.env.VITE_SIMPLICATE_TENANT_BASE_URL;
 if (!tenantURL) {
-  throw new Error('VITE_SIMPLICATE_TENANT_BASE_URL is not defined');
+  throw new Error('VITE_SIMPLICATE_TENANT_BASE_URL is not defined in .env.local');
 }
 if (!tenantURL.endsWith('/')) {
   tenantURL += '/';
 }
 
 const baseURL = `${tenantURL}/api/v2/`;
+
+if (import.meta.env.VITE_SIMPLICATE_API_KEY === undefined || import.meta.env.VITE_SIMPLICATE_API_SECRET === undefined) {
+  throw new Error('VITE_SIMPLICATE_API_KEY and/or VITE_SIMPLICATE_API_SECRET is not defined in .env.local');
+}
 
 export const axios = axiosPlugin.create({
   baseURL,
