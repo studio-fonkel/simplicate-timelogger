1fd74ada8cd5d4d99382ab04cc311f00b5a576e3
Anoesj <anoesjsadraee@gmail.com>
2021-12-08 18:11:26 +0100

    Always prepend scss with variable imports

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+const srcPath = path.resolve(__dirname, './src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use 'sass:math';
+          @import "${srcPath}/scss/variables.scss";
+        `,
+      },
+    },
+  },
 });
