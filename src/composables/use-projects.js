8deb635e563890117e453044dca218675b12e58c
Anoesj <anoesjsadraee@gmail.com>
2022-03-25 13:41:47 +0100

    Sort projects based on recent employee activity

--- src/composables/use-projects.js
+++ src/composables/use-projects.js
@@ -1,6 +1,6 @@
 import { ref, shallowRef } from 'vue';
 import { axios } from './use-axios.js';
-// import { currentEmployeeID } from './use-employees.js';
+import { currentEmployeeID } from './use-employees.js';
 import { POLLING_INTERVALS, registerCallback, unregisterCallback } from './use-polling.js';
 
 export const availableProjects = ref([]);
@@ -22,14 +22,70 @@ function addProjects (projects) {
 export async function fetchProjects () {
   loadingAvailableProjects.value = true;
 
-  const { data: projects } = await axios.get('projects/project', {
-    // params: {
-    //   'q[employee.id]': currentEmployeeID.value,
-    // },
-  });
+  let projects, latestLogs;
+
+  await Promise.all([
+    await axios.get('projects/project', {
+      params: {
+        // 'sort': '-modified',
+        // 'q[employee.id]': currentEmployeeID.value,
+      },
+    }).then(({ data }) => {
+      projects = data;
+    }),
+
+    await axios.get('hours/hours', {
+      params: {
+        'sort': '-start_date',
+        'limit': 30,
+        'q[employee.id]': currentEmployeeID.value,
+      },
+    }).then(({ data }) => {
+      latestLogs = data;
+    }),
+  ]);
+
+  const indexOfProjectInLatestLogs = (project) => {
+    return latestLogs.data.findIndex(log => log.project.id === project.id);
+  };
+
+  // Copy for earlier debugging purposes.
+  const projectsData = [...projects.data]
+    .sort((a, b) => {
+      const indexOfAInLatestLogs = indexOfProjectInLatestLogs(a);
+      const indexOfBInLatestLogs = indexOfProjectInLatestLogs(b);
+
+      if (indexOfAInLatestLogs > -1 && indexOfBInLatestLogs === -1) {
+        return -1;
+      }
+      else if (indexOfAInLatestLogs === -1 && indexOfBInLatestLogs > -1) {
+        return 1;
+      }
+      else if (indexOfAInLatestLogs === -1 && indexOfBInLatestLogs === -1) {
+        const modifiedA = a.modified,
+              modifiedB = b.modified;
+
+        if (modifiedA > modifiedB) {
+          return -1;
+        }
+        else if (modifiedA < modifiedB) {
+          return 1;
+        }
+        return 0;
+      }
+      else {
+        if (indexOfAInLatestLogs > indexOfBInLatestLogs) {
+          return 1;
+        }
+        else if (indexOfAInLatestLogs < indexOfBInLatestLogs) {
+          return -1;
+        }
+        return 0;
+      }
+    });
 
   clearProjects();
-  addProjects(projects.data);
+  addProjects(projectsData);
   loadingAvailableProjects.value = false;
 }
 
